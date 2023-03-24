9eb72eae22638ddce0c2decc4dd68b725dd524dd
AmitxParmar <amitparmar901@gmail.com>
2023-03-25 01:13:51 +0530

    minor changes:

--- src/context/post_context.tsx
+++ src/context/post_context.tsx
@@ -1,4 +1,6 @@
-import { useContext, createContext, useEffect, useState } from 'react'
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { useContext, createContext, useEffect } from 'react'
 
 type Post = {
     categoryId: string;
@@ -7,7 +9,7 @@ type Post = {
 }
 
 interface State {
-    createPost(post: Post): void;
+    createPost(post: Post): void | null;
 }
 const PostsContext = createContext<State | undefined>(undefined);
 
@@ -16,24 +18,28 @@ interface IPostProvider {
 }
 
 export const PostsProvider: React.FC<IPostProvider> = ({ children }) => {
-    const [posts, setPosts] = useState<[]>([]);
 
-    useEffect(() => {
-        localStorage.getItem('posts');
-    }, []);
 
-    /* const createPost = (post: Post): void => {
-        const getExistingPosts = () => {
-            const posts = localStorage.getItem('posts');
-            return posts ? JSON.parse(posts) : [];
-        }
-        existingArray.push(post);
-        localStorage.setItem('posts', JSON.stringify(existingArray));
-    }; */
+    const getExistingPosts = (): [] => {
+        const posts = localStorage.getItem('posts');
+        return posts ? JSON.parse(posts) : [];
+    }
+
+    const createPost = (post: Post): void | null => {
+        /* const existingArray: typeof getExistingPosts = () => getExistingPosts();
+return null
+        if (existingArray) {
+            existingArray.push(post);
+            localStorage.setItem('posts', JSON.stringify(existingArray));
+        } */
+        return null;
+    };
 
 
     return (
-       null
+        <PostsContext.Provider value={{ createPost }}>
+            {children}
+        </PostsContext.Provider>
     );
 };
 
