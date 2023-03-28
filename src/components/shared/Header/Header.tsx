3cdbca4a7c03d8019f79f6ce1a2854fe43b37dbf
AmitxParmar <amitparmar901@gmail.com>
2023-03-28 20:35:54 +0530

    fix:header

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.FC = () => {
         {/* NAV/HEADER */}
         <nav className="fixed right-0 left-0 top-0 z-[1040] flex flex-row flex-nowrap items-center justify-start bg-[linear-gradient(280.39deg,_#071530_64.59%,#405a86_109.59%)] py-1 px-2 shadow-[0_2px_8px_rgba(0,0,0,.12)] lg:h-14">
           {/* Flex-flow: row nowrap */}
-          <div className="m-auto flex flex-start w-full py-0 xl:pr-10 xl:pl-20 sm:p-0 md:max-w-[512px] md:p-0 lg:flex lg:max-w-[512px] lg:items-center lg:p-0">
+          <div className="m-auto flex flex-start w-full py-0 xl:pr-10 xl:pl-20 sm:p-0 md:max-w-[512px] md:p-0 lg:flex lg:max-w-[512px] xl:max-w-full lg:items-center lg:p-0">
             {/* ASIDE LOGO */}
             <SidebarOpen />
             {/* HEADER ITEMS */}
